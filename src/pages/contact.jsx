adf97b4660a73ddd51193e03ab81e0d796443495
torikallcode <torikalcontent@gmail.com>
2024-08-15 11:23:59 +0800

    modified contact

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -3,8 +3,15 @@ import React from 'react'
 
 export const Contact = () => {
   return (
-    <div>
-      <ContactLayout></ContactLayout>
+    <div className="App bg-hitam-0">
+      <ContactLayout />
+      {/* Content di bawah section */}
+      <div className="h-screen bg-hitam-0">
+        <p className="p-10">
+          Scroll down to see more content...
+        </p>
+      </div>
     </div>
+
   )
 }
